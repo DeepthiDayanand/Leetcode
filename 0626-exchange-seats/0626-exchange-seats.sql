cd9164b356969f01b187dceefba13075d184312f
Deepthi Dayanand <71063515+DeepthiDayanand@users.noreply.github.com>
2022-12-07 21:22:28 +0530

    Time: 688 ms (7.51%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,9 +1,11 @@
 # Write your MySQL query statement below
 select (
-    case when mod(id,2)=1 and 
+    case 
+    when mod(id,2)=1 and 
     id!=(select count(*) from seat)
-    then id+1
-    when mod(id,2)=0 then id-1
+        then id+1
+    when mod(id,2)=0 
+        then id-1
     else id
     end) 
 id, student from seat
