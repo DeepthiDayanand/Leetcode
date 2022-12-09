2ed60f61ed9e4c2471fd3eb72babaee63eb1c483
Deepthi Dayanand <71063515+DeepthiDayanand@users.noreply.github.com>
2022-12-09 22:26:33 +0530

    Time: 10 ms (56.52%), Space: 9.7 MB (80.27%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
@@ -18,10 +18,11 @@ public:
         max_value = max(max_value, root->val);
         min_value = min(min_value, root->val);
         
-        int left = pathDiff(root->left, max_value, min_value);
-        int right = pathDiff(root->right, max_value, min_value);
+//         int left = pathDiff(root->left, max_value, min_value);
+//         int right = pathDiff(root->right, max_value, min_value);
         
-        return max(left, right);
+//         return max(left, right);
+        return max(pathDiff(root->left, max_value, min_value), pathDiff(root->right, max_value, min_value));
     }
     
     int maxAncestorDiff(TreeNode* root) {
