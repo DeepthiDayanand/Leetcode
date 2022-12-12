cd8ad8aa389777360b4c9df38b5125167b3ac2fe
Deepthi Dayanand <71063515+DeepthiDayanand@users.noreply.github.com>
2022-12-12 12:25:52 +0530

    Time: 0 ms (100.00%), Space: 6 MB (55.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,11 +3,10 @@ public:
     int climbStairs(int n) {
         int step1 = 0, step2 = 1, ans = 0, steps = 0;
         
-        while(steps < n) {
+        for(steps = 0; steps < n; steps++) {
             ans = step1 + step2;
             step1 = step2;
             step2 = ans;
-            steps++;
         }
         return ans;
     }
