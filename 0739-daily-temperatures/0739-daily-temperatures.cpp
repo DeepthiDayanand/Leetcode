43db7ebcfc6a40ebb70750bc8494e1eb1555d8a6
Deepthi Dayanand <71063515+DeepthiDayanand@users.noreply.github.com>
2022-12-18 11:58:29 +0530

    Time: 202 ms (95.44%), Space: 103.1 MB (40.54%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
+    vector<int> dailyTemperatures(vector<int>& temp) {
         stack<int> s;
-        vector<int> waittime(temperatures.size());
+        vector<int> waittime(temp.size());
         
-        for(int i = 0; i < temperatures.size(); i++) {
-            while(!s.empty() && temperatures[i] > temperatures[s.top()]) 
+        for(int i = 0; i < temp.size(); i++) {
+            while(!s.empty() && temp[i] > temp[s.top()]) 
                 waittime[s.top()] = i - s.top(), s.pop();
             s.push(i);
         }
