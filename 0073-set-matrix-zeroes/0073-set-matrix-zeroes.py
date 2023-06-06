8a1e52165be1481c93bfab09a63013a0d738b3ab
Deepthi Dayanand <71063515+DeepthiDayanand@users.noreply.github.com>
2023-06-06 20:30:44 +0530

    Time: 138 ms (55.72%), Space: 17.1 MB (63.58%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,21 +4,15 @@ class Solution:
         n = len(matrix)
         m = len(matrix[0])
    
-        row = [0] * n  # row array
-        col = [0] * m  # col array
+        row = [0] * n  
+        col = [0] * m  
 
-    # Traverse the matrix:
         for i in range(n):
             for j in range(m):
                 if matrix[i][j] == 0:
-                # mark ith index of row wih 1:
                     row[i] = 1
-
-                # mark jth index of col wih 1:
                     col[j] = 1
 
-    # Finally, mark all (i, j) as 0
-    # if row[i] or col[j] is marked with 1.
         for i in range(n):
             for j in range(m):
                 if row[i] or col[j]:
