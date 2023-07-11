694b1fd597813e810ce73bf9e40092715148fdee
Deepthi Dayanand <71063515+DeepthiDayanand@users.noreply.github.com>
2023-07-11 21:07:58 +0530

    Time: 800 ms (47.99%), Space: 41.2 MB (93.18%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,17 +5,56 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        nums = []
         
-        while head:
-            nums.append(head.val)
-            head = head.next
+        slow, fast = head, head
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
             
-        l, r = 0, len(nums) - 1
-        while l <= r:
-            if nums[l] != nums[r]:
+        prev = None
+        #slow is now the midpoint, reverse the linked list from the midpoint
+        while slow:
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+        
+        l, r = head, prev
+        while r:
+            if l.val != r.val:
                 return False
-            l += 1
-            r -= 1
-            
+            l = l.next
+            r = r.next
         return True
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         ------Method 1-------
+#         <- Put the values of the linked list into an array and then use two pointers on the array ->
+#         nums = []
+        
+#         while head:
+#             nums.append(head.val)
+#             head = head.next
+            
+#         l, r = 0, len(nums) - 1
+#         while l <= r:
+#             if nums[l] != nums[r]:
+#                 return False
+#             l += 1
+#             r -= 1
+            
+#         return True
+
+        
+
