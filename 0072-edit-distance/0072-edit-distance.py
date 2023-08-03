7bc7b3b303954ed510ae14772ec97abb877e2799
Deepthi Dayanand <71063515+DeepthiDayanand@users.noreply.github.com>
2023-08-03 17:26:09 +0530

    Time: 115 ms (80.66%), Space: 20.1 MB (45.18%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,22 +1,19 @@
-class Solution(object):
-    def minDistance(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: int
-        """
-        matrix = [[float("inf")] * (len(word2) + 1) for i in range(len(word1) + 1)]
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
         
+        dp = [[float("inf")] * (len(word2) + 1) for i in range (len(word1) + 1)]
+        
+        for i in range(len(word1) + 1):
+            dp[i][len(word2)] = len(word1) - i
         for j in range(len(word2) + 1):
-            matrix[len(word1)][j] = len(word2) - j
-        for i in range (len(word1) + 1):
-            matrix[i][len(word2)] = len(word1) - i
+            dp[len(word1)][j] = len(word2) - j
             
-        for i in range(len(word1) -1, -1, -1):
+        for i in range(len(word1) - 1, -1, -1):
             for j in range(len(word2) - 1, -1, -1):
                 if word1[i] == word2[j]:
-                    matrix[i][j] = matrix[i+1][j+1]
+                    dp[i][j] = dp[i+1][j+1]
                 else:
-                    matrix[i][j] = 1 + min(matrix[i][j+1], matrix[i+1][j], matrix[i+1][j+1])
+                    dp[i][j] = 1 + min(dp[i][j+1], dp[i+1][j], dp[i+1][j+1])
                     
-        return matrix[0][0]
+        return dp[0][0]
+        
