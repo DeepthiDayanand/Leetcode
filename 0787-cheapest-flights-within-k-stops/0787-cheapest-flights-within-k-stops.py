9505343f539397f30c0178fbf8f1ab537a34070f
Deepthi Dayanand <71063515+DeepthiDayanand@users.noreply.github.com>
2023-08-28 23:13:48 +0530

    Time: 200 ms (37.58%), Space: 17.6 MB (61.21%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,10 +1,9 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        
         prices = [float("inf")] * n
         prices[src] = 0
         
-        for i in range (k + 1):
+        for i in range(k + 1):
             tempPrices = prices.copy()
             
             for s, d, p in flights:
@@ -13,8 +12,7 @@ class Solution:
                     
                 if prices[s] + p < tempPrices[d]:
                     tempPrices[d] = prices[s] + p
-                    
+            
             prices = tempPrices
         
         return -1 if prices[dst] == float("inf") else prices[dst]
-        
