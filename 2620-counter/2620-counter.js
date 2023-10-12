4713fdabae3aad40893914aecb8263661524681d
Deepthi Dayanand <71063515+DeepthiDayanand@users.noreply.github.com>
2023-10-12 13:37:06 +0530

    Time: 54 ms (31.99%), Space: 42 MB (29.65%) - LeetHub

--- 2620-counter/2620-counter.js
+++ 2620-counter/2620-counter.js
@@ -2,24 +2,21 @@
  * @param {number} n
  * @return {Function} counter
  */
-// var createCounter = function(n) {
-//     let count = n
-    
-//     return function() {
-//         return count++
-//     };
-// };
+
 
 var createCounter = function(n) {
+    let count = n
+    
     return function() {
-        return n++
-    }
-}
+        return count++
+    };
+};
+
 
 
 /** 
  * const counter = createCounter(10)
- * counter() // 10
+ * counter() // 10 || here 10 is a hidden state, sort of like a private variable
  * counter() // 11
  * counter() // 12
  */
